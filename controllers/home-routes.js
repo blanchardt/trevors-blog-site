b47abb29a6bcc189ab65454131b19c41b9b0a176
unknown <blancardt@merrimack.edu>
2023-10-04 19:18:15 -0400

    added the controller for the dashboard.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 
 //set up the homepage.
 router.get('/', async (req, res) => {
@@ -20,6 +21,26 @@ router.get('/', async (req, res) => {
     res.render('homepage', { 
       //blogs, 
       logged_in: req.session.logged_in, 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    // const userData = await User.findByPk(req.session.user_id, {
+    //   attributes: { exclude: ['password'] },
+    //   include: [{ model: Project }],
+    // });
+
+    // const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      //...user,
+      logged_in: true
     });
   } catch (err) {
     res.status(500).json(err);
