af7711b75f3c9ec273fbcdbfcd715feab16e96ee
unknown <blancardt@merrimack.edu>
2023-10-05 15:42:23 -0400

    added views for the comments and a route to view a page for creating a comment.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,22 +6,48 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     //Get all blogs and JOIN with user data
-    // const blogData = await Blog.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['name'],
-    //     },
-    //   ],
-    // });
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
     //Serialize data so the template can read it
-    //const blogs = blogData.map((blog) => blog.get({ plain: true }));
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
     //Pass serialized data and session flag into template
     res.render('homepage', { 
-      //blogs, 
+      blogs, 
       logged_in: req.session.logged_in, 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//set up the homepage.
+router.get('/comment/:id', withAuth, async (req, res) => {
+  try {
+    //Get all blogs and JOIN with user data
+    const blogData = await Blog.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'username'],
+        },
+      ],
+    });
+
+    //Serialize data so the template can read it
+    const blog = blogData.get({ plain: true });
+
+    //Pass serialized data and session flag into template
+    res.render('createComment', { 
+      ...blog, 
+      logged_in: true, 
     });
   } catch (err) {
     res.status(500).json(err);
